49459b7b0340e640794c3650ebd577ea058dcc08
jevay <jevay.liu@hotmail.com>
2015-12-14 08:15:04 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String content = "hello world";
+		HelloWorld.print(content);
+	}
+	
+	public static void print(String content) {
+		System.out.println(content);
+	}
+
+}
